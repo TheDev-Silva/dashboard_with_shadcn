d612e4c10475064a3a57027c76228c8a2aa18d4b
silvadeveloper2024@gmail.com <silvadeveloper2024@gmail.com>
2024-08-25 14:29:32 -0300

    feat: dropdown-menu e input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="w-full">
       <h1>germano silva</h1>
+      <Button >
+        Teste
+      </Button>
     </div>
   )
 }
